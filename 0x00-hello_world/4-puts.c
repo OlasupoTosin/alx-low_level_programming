5fbf12b83c547e467c7a03c12aed36eedad28c24
OlasupoTosin <olasupofrancis90@gmail.com>
2022-09-08 19:55:25 -0700

    4. Hello, Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,9 @@
 /**
  main - Uses puts function to print a line
  Description: We are unable to use print so we use the main function
- Return: 0
+ Return 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
